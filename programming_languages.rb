2e916b3ed7460d3595ae711d655dd5565b4bde9c
Pasquale Rocchio <patrocc6@gmail.com>
2017-05-05 22:57:11 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,16 @@
 def reformat_languages(languages)
-  # your code here
+  new_languages = {}
+  languages.each do |oo_or_functional, programming_languages|
+    programming_languages.each do |language, data|
+      new_languages[language] = {} if new_languages[language].nil?
+      if new_languages[language][:style].nil?
+        new_languages[language][:style] = []
+      end
+      new_languages[language][:style] << oo_or_functional
+      data.each do |key, value|
+        new_languages[language][key] = value
+      end
+    end
+  end
+  new_languages
 end
